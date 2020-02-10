ad4c80d7d8bd0dd5798b0047fe0df747e7102e1c
hhon <>
2020-02-09 19:16:26 -0800

    add check off finished item feature

--- src/List.js
+++ src/List.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function List (props) {
-    return (
-        <ul>
-            {
-                props.items.map(
-                (i, index) => {
-                return(
-                <div>
-                    <li key={index}>{i}<button onClick={()=>i.strike()}>Finish</button></li>
-                </div>
-                )
+class List extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            markCompleted: false
+        };
+    }
+
+    handleChange = () => {
+        let completed = this.state.markCompleted;
+        completed = completed ? false : true;
+        this.setState({markCompleted: completed})
+    }
+
+    getStyle = () => {
+        return {
+            textDecoration: this.state.markCompleted ? 'line-through' : 'none',
+        }
+    }
+
+    render () {
+        return (
+            <ul>
+                {
+                    this.props.items.map(
+                    (i, index) => {
+                    return(
+                    <div style={this.getStyle()}>
+                        <li key={index} markCompleted={this.state.markCompleted}>{i}<input type='checkbox' onChange={this.handleChange}></input></li>
+                    </div>
+                    )
+                    }
+                    )
                 }
-                )
-            }
-        </ul>
-    );
+            </ul>
+        );
+    }
 };
 
 export default List; 
