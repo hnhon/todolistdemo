e798a7c1ded8fee3ee8575649d2c0d69437d387e
hhon <>
2020-02-13 16:30:03 -0800

    refatore List.js

--- src/List.js
+++ src/List.js
@@ -2,80 +2,47 @@ import React from 'react';
 import './List.css';
 
 function List (props) {
+    const list = (i)=> {
+            return (
+                    <ul className='list' key={i.id}>
+                        <p id={i.done === true? "done": "active"}>
+                            {i.text}
+                        </p>
+                        <input type='checkbox' onClick={() => props.onClick(i.id)}></input>
+                        <i onClick={() => props.onDelete(i.id)}>Delete</i>
+                    </ul>
+            )
+    }
 
     if (props.filter==='All') {
         return (
             <div>
                 {
-                    props.items.map(
-                        (i) => {
-                            return (
-                                    <ul className='list' key={i.id}>
-                                        <p style={{
-                                        textDecoration: i.done
-                                          ? "line-through"
-                                          : ""
-                                        }}>{i.text}</p>
-                                        <input type='checkbox' onClick={() => props.onClick(i.id)}></input>
-                                        <i onClick={() => props.onDelete(i.id)}>Delete</i>
-                                    </ul>
-                            )
-                        }
-                    )
+                    props.items.map(i => list(i))
                 }
             </div>
         );
     } else if (props.filter==='Active') {
         let newList = props.items.filter(
-            (item)=>{
-                return item.done === false;
-            }
+            item => {return item.done === false;}
         );
+
         return (
             <div>
                 {
-                    newList.map(
-                        (i) => {
-                            return (
-                                    <ul className='list' key={i.id}>
-                                        <p style={{
-                                        textDecoration: i.done
-                                          ? "line-through"
-                                          : ""
-                                        }}>{i.text}</p>
-                                        <input type='checkbox' onClick={() => props.onClick(i.id)}></input>
-                                        <i onClick={() => props.onDelete(i.id)}>Delete</i>
-                                    </ul>
-                            )
-                        }
-                    )
+                    newList.map(i =>list(i))
                 }
             </div>
         );
     } else {
         let newList = props.items.filter(
-            (item)=>{
-                return item.done === true;
-            }
+            item => {return item.done === true;}
         );
+
         return (
             <div>
                 {
-                    newList.map(
-                        (i) => {
-                            return (
-                                    <ul className='list' key={i.id}>
-                                        <p style={{
-                                        textDecoration: i.done
-                                          ? "line-through"
-                                          : ""
-                                        }}>{i.text}</p>
-                                        <input type='checkbox' onClick={() => props.onClick(i.id)}></input>
-                                        <i onClick={() => props.onDelete(i.id)}>Delete</i>
-                                    </ul>
-                            )
-                        }
-                    )
+                    newList.map(i => list(i))
                 }
             </div>
         );
