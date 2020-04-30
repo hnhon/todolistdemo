9cb28f34f09a8990fcd4b73c09b1a7d3f66f74de
hhon <>
2020-04-30 08:19:59 -0700

    Updates

---
+++ precache-manifest.7093e6c10e9cb5e193d1b727acebcdf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e033d24c4fd8edc1dedbba4055432d18",
+    "url": "/todolistdemo/index.html"
+  },
+  {
+    "revision": "27d6464cdc13c3d71aa6",
+    "url": "/todolistdemo/static/css/main.33112b1c.chunk.css"
+  },
+  {
+    "revision": "5328cc4d6f9e87fa6b1f",
+    "url": "/todolistdemo/static/js/2.2b099d8c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todolistdemo/static/js/2.2b099d8c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27d6464cdc13c3d71aa6",
+    "url": "/todolistdemo/static/js/main.f0fbae39.chunk.js"
+  },
+  {
+    "revision": "22d31f2c72de82b09224",
+    "url": "/todolistdemo/static/js/runtime-main.e4c2d420.js"
+  }
+]);
